2bd4086562e53bc3904eeb1b6765efe3497d4b5b
Michse017 <severichecastellano@gmail.com>
2025-04-23 23:10:44 -0500

    Update .gitignore, MySQL playbook, Terraform configuration, and add LICENSE file

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,35 +1,48 @@
 provider "azurerm" {
   features {}
+  subscription_id = "bf177a1c-c4b4-4da4-b0c3-11cf21bcdc6e"
+}
+
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "local_file" "ssh_key_pem" {
+  content         = tls_private_key.ssh_key.private_key_openssh
+  filename        = "${path.module}/ssh_key.pem"
+  file_permission = "0600"
 }
 
 resource "azurerm_resource_group" "main" {
-  name     = "rg-tutorial"
+  name     = "rg-mysql-app"
   location = "East US"
 }
 
 resource "azurerm_virtual_network" "main" {
-  name                = "vnet-tutorial"
+  name                = "vnet-mysql-app"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 }
 
 resource "azurerm_subnet" "main" {
-  name                 = "subnet-tutorial"
+  name                 = "subnet-mysql"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
 resource "azurerm_public_ip" "main" {
-  name                = "pip-tutorial"
+  name                = "pip-mysql"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
+  sku                 = "Standard"
 }
 
 resource "azurerm_network_interface" "main" {
-  name                = "nic-tutorial"
+  name                = "nic-mysql"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
 
@@ -47,7 +60,6 @@ resource "azurerm_linux_virtual_machine" "mysql" {
   location            = azurerm_resource_group.main.location
   size                = "Standard_B1s"
   admin_username      = "azureuser"
-  admin_password      = "Password1234!" # Usa variables seguras en producción
   network_interface_ids = [
     azurerm_network_interface.main.id,
   ]
@@ -61,8 +73,80 @@ resource "azurerm_linux_virtual_machine" "mysql" {
     sku       = "18.04-LTS"
     version   = "latest"
   }
+  admin_ssh_key {
+    username   = "azureuser"
+    public_key = file("~/.ssh/id_rsa.pub")
+  }
+  disable_password_authentication = true
+
+  depends_on = [local_file.ssh_key_pem]
+
+  provisioner "local-exec" {
+    command = "sleep 50 && ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i '${azurerm_public_ip.main.ip_address},' --private-key=~/.ssh/id_rsa --user=azureuser --ssh-common-args='-o StrictHostKeyChecking=no' ../ansible/mysql_playbook.yml"
+  }
+}
+
+data "http" "my_ip" {
+  url = "https://api.ipify.org"
+}
+
+locals {
+  controller_ip     = "${chomp(data.http.my_ip.response_body)}/32"
+  appservice_subnet = "10.0.2.0/24" // Cambia esto si tu App Service tiene otra subred
+}
+
+resource "azurerm_network_security_group" "mysql_nsg" {
+  name                = "nsg-mysql"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "Allow-SSH-Controller"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_address_prefix      = local.controller_ip
+    destination_port_range     = "22"
+    source_port_range          = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Allow-MySQL-AppService"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_address_prefix      = local.appservice_subnet
+    destination_port_range     = "3306"
+    source_port_range          = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Deny-All-Inbound"
+    priority                   = 200
+    direction                  = "Inbound"
+    access                     = "Deny"
+    protocol                   = "*"
+    source_address_prefix      = "*"
+    destination_port_range     = "*"
+    source_port_range          = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "mysql_nic_nsg" {
+  network_interface_id      = azurerm_network_interface.main.id
+  network_security_group_id = azurerm_network_security_group.mysql_nsg.id
 }
 
 output "mysql_vm_public_ip" {
   value = azurerm_public_ip.main.ip_address
+}
+
+output "ssh_private_key_pem" {
+  value     = tls_private_key.ssh_key.private_key_pem
+  sensitive = true
 }
